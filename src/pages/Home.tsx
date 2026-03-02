7766fec6215e913efb091df0faaf01990bb74c95
travelmadura323-hue <travelmadura323@gmail.com>
2026-03-02 14:55:34 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -394,9 +394,9 @@ import Chairmanimage from '/images/VKT-Balan.png';
                 <img
                   src="/images/VKT-BALAN.png"
                   alt="Chairman V.K.T Balan"
-                  // className="w-80 h-80 object-cover rounded-2xl shadow-2xl border-4 border-white"
+                  className="w-80 h-80 object-cover rounded-2xl shadow-2xl border-4 border-white"
                 />
-                <div className="absolute -bottom-6 -right-6 w-48 h-48 bg-accent rounded-2xl -z-0" />
+                {/* <div className="absolute -bottom-6 -right-6 w-48 h-48 bg-accent rounded-2xl -z-0" /> */}
               </div>
             </motion.div>
             <motion.div
