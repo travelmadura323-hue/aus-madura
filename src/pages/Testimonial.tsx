8a5f9e8ddf92dbba748a00354a3d6504ff6a2e7d
unknown <travelmadura323@gmail.com>
2026-03-10 12:26:15 +0530

    Update Testimonial.tsx

--- src/pages/Testimonial.tsx
+++ src/pages/Testimonial.tsx
@@ -17,49 +17,49 @@ const testimonials = [
     name: "Mr.YB Wong Hon Wai",
     Designation: "Minister of Tourism, Malaysia",
     text: "I'm Happy For the Arrangement by the Madura Travel Service For past two weeks.I have the opportunityto travel to a few places an interesting Attraction one other the Unesco monuments Chennai .I m happy with the services rendered Thank you",
-    image: gal["images/YB.jpg"] as string
+    image: gal["/images/YB.jpg"] as string
   },
   {
     name: "Mr. Anbil Mahesh",
     designation: "Minister for Education - Government of Tamilnadu",
     rating: 5,
     image: cel["/images/celebrity/Anbil mahesh.jpg"] as string,
-    text: "I extend my heartfelt thanks to the entire Madura Travel Service team for their professional assistance in organizing international trips for the students of Tamil Nadu's government schools..."
+    text: "I extend my heartfelt thanks to the entire Madura Travel Service team for their professional assistance in organizing international trips for the students of Tamil Nadu's government schools, helping them realize their dreams. Traveling with the students made me feel like a young boy again, as I thoroughly enjoyed the beautifully planned tours to destinations like Singapore, Malaysia, Japan, South Korea, and more. Kudos to Madura Travel Service for their incredible efforts."
   },
   {
     name: "Mr. Napoleon",
     designation: "Cine Actor & Politician",
     rating: 5,
     image: cel["/images/celebrity/nepolean.jpg"] as string,
-    text: "Mr. Sriharan Balan and his exceptional team provided seamless service..."
+    text: "Mr. Sriharan Balan and his exceptional team provided seamless service, taking on the monumental task of organizing my son’s wedding in Tokyo, Japan, in November 2024, with absolute ease. Every guest was treated like a VIP from start to finish, ensuring a memorable and stress-free experience for all involved."
   },
   {
     name: "Mr. Kamal Haasan",
     designation: "Cine Actor & Director",
     rating: 5,
     image: cel["/images/celebrity/Kamalhasan.jpg"] as string,
-    text: "Mr. V.K.T. Balan was more than just a travel consultant..."
+    text: "Mr. V.K.T. Balan was more than just a travel consultant; he was a cherished friend and pillar of support throughout my decades-long journey in cinema, right from my early days. His guidance and expertise enriched numerous travel programs and shoots. I extend my heartfelt wishes for continued success and prosperity to his entire team."
   },
   {
     name: "Mr. Venkatesh Bhat",
     designation: "TCDC Fame & CEO, Accord Hotels",
     rating: 5,
     image: cel["/images/celebrity/Venkatesh-Bhat.jpg"] as string,
-    text: "My long-standing association with Madura Travel Service has made my global travels seamless..."
+    text: "My long-standing association with Madura Travel Service has made my global travels seamless and stress-free. Their expertise in handling visas ensures timely approvals without any delays, making them my trusted travel partner. Truly exceptional service every time!"
   },
   {
     name: "Mrs. P. Susheela",
     designation: "Legendary Singer",
     rating: 5,
     image: cel["/images/celebrity/susheela.jpg"] as string,
-    text: "My journey with Madura Travel Service began when Mr. VKT Balan helped me obtain my first passport..."
+    text: "My journey with Madura Travel Service began when Mr. VKT Balan helped me obtain my first passport. Since then, he has been a constant support, helping me travel the world and share my voice globally. He is like a son to me, and Madura Travel Service feels like family."
   },
   {
     name: "Mr. Sandy",
     designation: "Dance Master",
     rating: 5,
     image: cel['/images/celebrity/sandy.jpg'] as string,
-    text: "Mr. Sriharan Balan has been a tremendous support during my international shows..."
+    text: "Mr. Sriharan Balan has been a tremendous support during my international shows. His professional team is always available 24/7, ensuring that my travel experiences are smooth and enjoyable. I truly appreciate their dedication and commitment to making every journey a seamless and positive experience"
   },
   {
     name: "Mr.Vijay",
